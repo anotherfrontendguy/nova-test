d338d1b251ed064f3fa7b4d3b7bd57ef0dc85100
Johannes Rückert <rawbauke@gmail.com>
2020-06-03 14:54:03 +0200

    init

---
+++ database/migrations/2020_06_03_105916_create_downloads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDownloadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('category_id');
+
+            $table->string('file')->nullable();
+            $table->string('file_name')->nullable();
+            $table->unsignedInteger('file_size')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('downloads');
+    }
+}
